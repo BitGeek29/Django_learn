56f4fa927bea05d21a9cbf0e5e03f4da7dceb6fc
Haseeb Shaikh <haseeb.shaikh@ggcollege.in>
2022-10-12 12:01:34 +0000

    test djongo

--- dbJang/mongodb/models.py
+++ dbJang/mongodb/models.py
@@ -1,4 +1,23 @@
-from django.db import models
+#from django.db import models
 
 # Create your models here.
-#dddddddddd
+from djongo import models
+import json
+
+
+class Report(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        abstract = True
+
+class Entry(models.Model):
+    report = models.ArrayField(
+        model_container=Report
+    )    
+    headline = models.CharField(max_length=255)    
+
+e = Entry()
+e.report = json.load(open('/workspace/Django_learn/Datasets/covid19.json'))
+e.headline = 'Djongo is the best Django and MongoDB connector'
+e.save()
